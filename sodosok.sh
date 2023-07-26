42dc562cd92d67fddd106102bd3da0e158201788
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-26 12:07:02 +0700

    Add files via upload

---
+++ sodosok.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+#shadowsocks-libev obfs install by AnuyaBazoelk
+# My Telegram : https://t.me/AnuyBazoelk
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Jengkolonline/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
+# Link Hosting Kalian
+bzvpn="raw.githubusercontent.com/Jengkolonline/shadowsocks-libev/main"
+
+source /etc/os-release
+OS=$ID
+ver=$VERSION_ID
+
+#Install_Packages
+echo "#############################################"
+echo "Install Paket..."
+apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake -y
+echo "Install Paket Selesai."
+echo "#############################################"
+
+
+#Install_Shadowsocks_libev
+echo "#############################################"
+echo "Install Shadowsocks-libev..."
+apt-get install software-properties-common -y
+if [[ $OS == 'ubuntu' ]]; then
+apt install shadowsocks-libev -y
+apt install simple-obfs -y
+elif [[ $OS == 'debian' ]]; then
+if [[ "$ver" = "9" ]]; then
+echo "deb http://deb.debian.org/debian stretch-backports main" | tee /etc/apt/sources.list.d/stretch-backports.list
+apt update
+apt -t stretch-backports install shadowsocks-libev -y
+apt -t stretch-backports install simple-obfs -y
+elif [[ "$ver" = "10" ]]; then
+echo "deb http://deb.debian.org/debian buster-backports main" | tee /etc/apt/sources.list.d/buster-backports.list
+apt update
+apt -t buster-backports install shadowsocks-libev -y
+apt -t buster-backports install simple-obfs -y
+fi
+fi
+echo "Install Shadowsocks-libev Selesai."
+echo "#############################################"
+
+#Server konfigurasi
+echo "#############################################"
+echo "Konfigurasi Server."
+cat > /etc/shadowsocks-libev/config.json <<END
+{   
+    "server":"0.0.0.0",
+    "server_port":8488,
+    "password":"tes",
+    "timeout":60,
+    "method":"aes-256-cfb",
+    "fast_open":true,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+}
+END
+echo "#############################################"
+
+#mulai ~shadowsocks-libev~ server
+echo "#############################################"
+echo "mulai ss server"
+systemctl enable shadowsocks-libev.service
+systemctl start shadowsocks-libev.service
+echo "#############################################"
+
+#buat client config
+echo "#############################################"
+echo "buat config obfs"
+cat > /etc/shadowsocks-libev.json <<END
+{
+    "server":"127.0.0.1",
+    "server_port":8388,
+    "local_port":1080,
+    "password":"",
+    "timeout":60,
+    "method":"chacha20-ietf-poly1305",
+    "mode":"tcp_and_udp",
+    "fast_open":true,
+    "plugin":"/usr/bin/obfs-local",
+    "plugin_opts":"obfs=tls;failover=127.0.0.1:1443;fast-open"
+}
+END
+chmod +x /etc/shadowsocks-libev.json
+echo "#############################################"
+
+echo -e "">>"/etc/shadowsocks-libev/akun.conf"
+
+echo "#############################################"
+echo "Menambahkan Perintah Shadowsocks-libev"
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2443:3543 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+ip6tables-save > /etc/ip6tables.up.rules
+cd /usr/bin
+wget -O addss "https://${bzvpn}/addss.sh"
+wget -O delss "https://${bzvpn}/delss.sh"
+wget -O cekss "https://${bzvpn}/cekss.sh"
+wget -O renewss "https://${bzvpn}/renewss.sh"
+chmod +x addss
+chmod +x delss
+chmod +x cekss
+chmod +x renewss
+cd
+rm -f /root/sodosok.sh
